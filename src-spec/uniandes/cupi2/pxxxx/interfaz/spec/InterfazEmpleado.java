e7f44124026db6d49745893dabd654871475ff6b
maveme <help@genmymodel.com>
2015-10-04 21:57:39 +0000

    commit by maveme using genmymodel: v20151004_215738

---
+++ src-spec/uniandes/cupi2/pxxxx/interfaz/spec/InterfazEmpleado.java
@@ -0,0 +1,21 @@
+package uniandes.cupi2.pxxxx.interfaz.spec;
+
+import co.edu.uniandes.cupi2.generador.anotaciones.interfaz.*;
+
+
+@UIFrame(alto=800, ancho=600, titulo = "Empleado")
+public class InterfazEmpleado extends _InterfazEmpleado
+{
+	private PanelExtensiones panelExtensiones;
+	
+	private Empleado mundo_Empleado;
+	
+	
+
+	public InterfazEmpleado()
+	 {
+
+	 }
+
+	
+}
---
+++ src-spec/uniandes/cupi2/pxxxx/mundo/spec/Fecha.java
@@ -0,0 +1,16 @@
+package uniandes.cupi2.pxxxx.mundo.spec;
+
+import co.edu.uniandes.cupi2.generador.anotaciones.*;
+
+
+public class Fecha extends _Fecha
+{
+	@Atributo(genGet=true, genSet=true)
+	private String dia;
+	
+
+	
+
+	
+}
+

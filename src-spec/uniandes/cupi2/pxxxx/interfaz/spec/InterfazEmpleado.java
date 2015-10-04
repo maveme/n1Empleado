be7fe8792187fb013da71754265bd5fac8b3eed3
maveme <help@genmymodel.com>
2015-10-04 23:01:10 +0000

    commit by maveme using genmymodel: v20151004_230109

---
+++ src-spec/uniandes/cupi2/pxxxx/interfaz/spec/InterfazEmpleado.java
@@ -0,0 +1,42 @@
+package uniandes.cupi2.pxxxx.interfaz.spec;
+
+import co.edu.uniandes.cupi2.generador.anotaciones.interfaz.*;
+import uniandes.cupi2.pxxxx.mundo.*;
+
+
+@UIFrame(alto=800, ancho=600, titulo = "Empleado")
+public class InterfazEmpleado extends _InterfazEmpleado
+{
+	private PanelExtension panelExtensiones;
+	
+	private Empleado mundo_Empleado;
+	
+	
+
+	public InterfazEmpleado()
+	 {
+
+	 }
+
+	
+
+
+    /**
+     * Método para la extensión 1
+     */
+    public void reqFuncOpcion1( )
+     {
+        //String resultado = "";
+        JOptionPane.showMessageDialog( this, resultado, "Respuesta", JOptionPane.INFORMATION_MESSAGE );
+     }
+
+    /**
+     * Método para la extensión 2
+     */
+    public void reqFuncOpcion2( )
+     {
+        String resultado = "";
+        JOptionPane.showMessageDialog( this, resultado, "Respuesta", JOptionPane.INFORMATION_MESSAGE );
+     }
+
+}
